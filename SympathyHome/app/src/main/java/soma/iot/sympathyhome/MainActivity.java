53de1f73a2d5933d7000d5a5e606e2f75bee3347
seojunkyo <seojk2me@gmail.com>
2015-08-10 00:29:52 +0900

    Create activity_main.xml layout

--- SympathyHome/app/src/main/java/soma/iot/sympathyhome/MainActivity.java
+++ SympathyHome/app/src/main/java/soma/iot/sympathyhome/MainActivity.java
@@ -1,11 +1,11 @@
 package soma.iot.sympathyhome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
